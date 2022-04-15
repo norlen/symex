bccf542fd2a26af550ce0978d8869baece6ccfb9
Joacim Norlén <joacim.norlen@gmail.com>
2022-04-15 14:06:08 +0200

    remove unused

--- src/vm/mod.rs
+++ src/vm/mod.rs
@@ -8,24 +8,3 @@ pub use error::{Result, VMError};
 pub use globals::*;
 pub use state::*;
 pub use vm::*;
-
-use crate::BV;
-
-enum Address {
-    Concrete(u64),
-    Symbolic(BV),
-}
-
-impl Address {}
-
-impl From<u64> for Address {
-    fn from(ptr: u64) -> Self {
-        Self::Concrete(ptr)
-    }
-}
-
-impl From<BV> for Address {
-    fn from(bv: BV) -> Self {
-        Self::Symbolic(bv)
-    }
-}
