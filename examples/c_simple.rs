eb6da2d4bd35ca8206ca766de527cd019174660a
Joacim Norlén <joacim.norlen@gmail.com>
2022-04-27 13:49:05 +0200

    rename examples

---
+++ examples/c_simple.rs
@@ -0,0 +1,13 @@
+use env_logger::init;
+use x0001e::{project::Project, vm::VM};
+
+fn main() {
+    init();
+
+    let project = Project::from_path("./samples/simple/main.bc").unwrap();
+    let vm = VM::new("main", &project).unwrap();
+
+    for path in vm {
+        println!("Path result: {:?}", path);
+    }
+}
--- examples/simple.rs
+++ examples/simple.rs
@@ -1,13 +1,20 @@
-use env_logger::init;
-use x0001e::{project::Project, vm::VM};
+use x0001e::{assume, symbolic};
+
+fn rust_simple_test(mut t: u32) -> u32 {
+    assume(t > 0);
+    if t == 0 {
+        // This path should never happen.
+        panic!("does not work");
+    }
+    symbolic(&mut t);
+    if t == 0 {
+        // But this should.
+        0
+    } else {
+        11
+    }
+}
 
 fn main() {
-    init();
-
-    let project = Project::from_path("./samples/simple/main.bc").unwrap();
-    let vm = VM::new("main", &project).unwrap();
-
-    for path in vm {
-        println!("Path result: {:?}", path);
-    }
+    rust_simple_test(6);
 }
