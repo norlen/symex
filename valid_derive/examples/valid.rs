f4d0f37ba4829ca7a993bb7f86e37dad1fa8bc59
Per Lindgren <per.lindgren@ltu.se>
2022-05-11 20:49:26 +0200

    enum validation, works (kind of)

--- valid_derive/examples/valid.rs
+++ valid_derive/examples/valid.rs
@@ -14,7 +14,6 @@ trait IsValid {
     fn is_valid(&self) -> bool;
 }
 
-#[derive(Validate)]
 enum Enum {
     Unit,
     Unnamed(u32),
@@ -22,38 +21,37 @@ enum Enum {
     Named { x: u32, y: Enum2 },
 }
 
-#[derive(Validate)]
 enum Enum2 {
     U1,
     U2,
 }
 
-// impl IsValid for Enum {
-//     fn is_valid(&self) -> bool {
-//         if let Enum::Unit = self {
-//             true
-//         } else if let Enum::Unnamed(e) = self {
-//             e.is_valid()
-//         } else if let Enum::Unnamed2(e) = self {
-//             e.is_valid()
-//         } else if let Enum::Named { x, y } = self {
-//             x.is_valid() && y.is_valid()
-//         } else {
-//             false
-//         }
-//     }
-// }
+impl IsValid for Enum {
+    fn is_valid(&self) -> bool {
+        if let Enum::Unit = self {
+            true
+        } else if let Enum::Unnamed(e) = self {
+            e.is_valid()
+        } else if let Enum::Unnamed2(e) = self {
+            e.is_valid()
+        } else if let Enum::Named { x, y } = self {
+            x.is_valid() && y.is_valid()
+        } else {
+            false
+        }
+    }
+}
 
-// impl IsValid for Enum2 {
-//     fn is_valid(&self) -> bool {
-//         if let Enum2::U1 = self {
-//             true
-//         } else if let Enum2::U2 = self {
-//             true
-//         } else {
-//             false
-//         }
-//     }
-// }
+impl IsValid for Enum2 {
+    fn is_valid(&self) -> bool {
+        if let Enum2::U1 = self {
+            true
+        } else if let Enum2::U2 = self {
+            true
+        } else {
+            false
+        }
+    }
+}
 
 fn main() {}
