a529f93f6df6459cd14c5e1850c34eb9325d07f7
Per Lindgren <per.lindgren@ltu.se>
2022-05-11 14:43:52 +0200

    validate_derive, wip

---
+++ valid_derive/examples/valid.rs
@@ -0,0 +1,9 @@
+use valid_derive::*;
+
+#[derive(Validate)]
+enum Enum {
+    A,
+    B,
+}
+
+fn main() {}
---
+++ valid_derive/src/lib.rs
@@ -0,0 +1,11 @@
+extern crate proc_macro;
+
+use proc_macro::TokenStream;
+use syn::
+
+#[proc_macro_derive(Validate)]
+pub fn hello_macro_derive(input: TokenStream) -> TokenStream {
+    // Construct a representation of Rust code as a syntax tree
+    // that we can manipulate
+    input
+}
