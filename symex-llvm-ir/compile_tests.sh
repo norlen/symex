492c379d0a446f810cd10ee032de44e7e42e8f24
norlen <joacim.norlen@gmail.com>
2023-09-27 17:17:56 +0200

    move symex to own folder

--- symex-llvm-ir/compile_tests.sh
+++ symex-llvm-ir/compile_tests.sh
@@ -3,7 +3,7 @@
 SCRIPT_DIR="$(dirname $BASH_SOURCE[0])"
 
 # Compile all .c files.
-for file in $(find $SCRIPT_DIR/../tests -name '*.c'); do
+for file in $(find $SCRIPT_DIR/tests -name '*.c'); do
     echo "generating files for $file"
     clang -c -emit-llvm $file -o ${file%.*}.bc && clang -S -emit-llvm $file -o ${file%.*}.ll
 done
@@ -11,21 +11,21 @@ done
 opts="-C panic=abort -C link-dead-code=yes" # -C debuginfo=2"
 
 # Compile all .rs files used in tests.
-for file in $(find $SCRIPT_DIR/../tests/samples -name '*.rs'); do
+for file in $(find $SCRIPT_DIR/tests/samples -name '*.rs'); do
     echo "generating files for $file"
     rustc --emit=llvm-bc --edition=2021 -o ${file%.*}.bc $opts $file
     rustc --emit=llvm-ir --edition=2021 -o ${file%.*}.ll $opts $file
 done
 
 # Compile all .rs files used in doctests.
-for file in $(find $SCRIPT_DIR/../tests/doc_tests -name '*.rs'); do
+for file in $(find $SCRIPT_DIR/tests/doc_tests -name '*.rs'); do
     echo "generating files for $file"
     rustc --emit=llvm-bc --edition=2021 -o ${file%.*}.bc $opts $file
     rustc --emit=llvm-ir --edition=2021 -o ${file%.*}.ll $opts $file
 done
 
 # Compile .ll files used by unit tests
-for file in $(find $SCRIPT_DIR/../tests/unit_tests -name '*.ll'); do
+for file in $(find $SCRIPT_DIR/tests/unit_tests -name '*.ll'); do
     echo "generating files for $file"
     llvm-as $file
 done
